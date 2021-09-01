48e4ad05fa629e38cf2f1e64b3a44f745183e00b
park0503 <hooo0503@gmail.com>
2021-09-01 23:07:16 +0900

    complet chapter 9

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
+    plugins: [new MiniCssExtractPlugin({
+        filename: "css/styles.css",
+    })],
     entry: "./src/client/js/main.js",
     mode: 'development',
+    watch: true,
     output: {
-        filename: "main.js",
+        filename: "js/main.js",
         //path는 무조건 절대경로
-        path: path.resolve(__dirname, "assets", "js"),
+        path: path.resolve(__dirname, "assets"),
+        clean: true,
     },
     module: {
         rules: [
@@ -23,9 +29,10 @@ module.exports = {
                 test: /\.scss$/,
                 //loader들을 합칠 땐 역순으로 합친다. 역순으로 실행되기 때문.
                 //sass-loader: sass파일을 css로 변환
-                //css-loader: css파일을 js에 가져올 수 있게(import) -> js가 웹사이트에 컴파일된 css를 입력하게 해 줌.
-                //style-loader: 웹사이트에 입력된 컴파일된 css를 실제 적용하게 해 줌. 실제 head에 style로 적용시켜줌.
-                use: ["style-loader", "css-loader", "sass-loader"]
+                //css-loader: css파일을 js에 import 해 줌.
+                //style-loader: import된 컴파일된 css를 실제 적용하게 해 줌. 실제 head에 style로 적용시켜 줌.
+                //MiniCssExtractPlugin: import된 컴파일된 css를 독립된 css파일로 분리해 줌.
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
             }
         ]
     }
