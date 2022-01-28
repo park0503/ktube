c0777fe1cee8a8806eb572b31cab171d8038b4d2
park0503 <hooo0503@gmail.com>
2022-01-29 02:41:17 +0900

    11.2

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -3,7 +3,7 @@ const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
 
 const handlePlayClick = (e) => {
     //비디오가 재생되고 있다면 멈춤
@@ -13,21 +13,18 @@ const handlePlayClick = (e) => {
         video.pause();
     }
     //아니라면 비디오 재생
+    playBtn.innerText = video.paused ? "Play" : "Paused"
 };
 
 const handleMute = (e) => {
-
+    if (video.muted) {
+        video.muted = false;
+    } else {
+        video.muted = true;
+    }
+    muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+    volumeRange.value = video.muted ? 0 : 0.5;
 };
-
-const handlePause = () => {
-    playBtn.innerText = "Play"
-}
-
-const handlePlay = () => {
-    playBtn.innerText = "Pause"
-}
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
-video.addEventListener("pause", handlePause);
-video.addEventListener("play", handlePlay);
