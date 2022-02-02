61660ee31711e82d12289fc7035e2299f5ec0dc9
park0503 <hooo0503@gmail.com>
2022-02-02 14:41:30 +0900

    13.1

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,11 +1,13 @@
 const startBtn = document.getElementById("startBtn");
+const preview = document.getElementById("preview");
 
 const handleStart = async () => {
     const stream = await navigator.mediaDevices.getUserMedia({
         audio: true,
         video: true
     });
-    console.log(stream);
+    preview.srcObject = stream;
+    preview.play();
 };
 
 startBtn.addEventListener("click", handleStart);
