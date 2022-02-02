743c031ed33e8420a6f397b200cb59dd40cfdaae
park0503 <hooo0503@gmail.com>
2022-02-02 15:30:23 +0900

    complete chapter 13

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -9,7 +9,7 @@ const handleDownload = () => {
     const a = document.createElement("a");
     a.href = videoFile;
     a.download = "K-Tube_Recording.webm";
-    document.body.appendChild(a);
+    document.body.appendChild(a); //이렇게 실제 html 문서 상 추가를 하지 않고 다운로드를 하는 것은 브라우저 차원에서 금지함. 보안상 이유인듯.
     a.click();
     a.remove();
 };
@@ -30,7 +30,7 @@ const handleStart = () => {
     recorder = new MediaRecorder(stream, {
         mimeType: "video/webm"
     });
-    recorder.ondataavailable = (e) => {
+    recorder.ondataavailable = (e) => { //recorder stop시 발생하는 이벤트
         //console.log(e.data); //e.data는 파일
         videoFile = URL.createObjectURL(e.data); //브라우저의 메모리상의 위치를 가르키는 url
         preview.srcObject = null;
@@ -49,7 +49,7 @@ const init = async () => {
         audio: true,
         video: true
     });
-    preview.srcObject = stream;
+    preview.srcObject = stream; // stream은 Object기에 src가 아닌 srcObject 속성에 넣어줘야 함
     preview.play();
 };
 
