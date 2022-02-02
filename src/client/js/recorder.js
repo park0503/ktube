f4765c630c1d6f93b02175df55db2130862dc118
park0503 <hooo0503@gmail.com>
2022-02-02 14:55:06 +0900

    13.2

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,13 +1,41 @@
 const startBtn = document.getElementById("startBtn");
 const preview = document.getElementById("preview");
 
-const handleStart = async () => {
-    const stream = await navigator.mediaDevices.getUserMedia({
+let stream = null;
+
+
+const handleStop = () => {
+    startBtn.innerText = "Start Recording";
+    startBtn.removeEventListener("click", handleStop);
+    startBtn.addEventListener("click", handleStart);
+};
+
+const handleStart = () => {
+    startBtn.innerText = "Stop Recording";
+    startBtn.removeEventListener("click", handleStart);
+    startBtn.addEventListener("click", handleStop);
+
+    const recorder = new MediaRecorder(stream);
+    recorder.ondataavailable = (e) => {
+        console.log(e);
+    };
+    console.log(recorder);
+    recorder.start();
+    console.log(recorder);
+    setTimeout(() => {
+        recorder.stop();
+    }, 10000);
+};
+
+const init = async () => {
+    stream = await navigator.mediaDevices.getUserMedia({
         audio: true,
         video: true
     });
     preview.srcObject = stream;
     preview.play();
 };
+
+init();
 
 startBtn.addEventListener("click", handleStart);
